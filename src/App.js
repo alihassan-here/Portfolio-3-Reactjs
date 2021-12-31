fd84cbff02a64ff0e8019607aebd52478499109e
unknown <alihassanuog50@gmail.com>
2022-01-01 01:15:58 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from'./components/Navbar';
+import Homepage from './components/Homepage';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Connect from './components/Connect';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Homepage/>
+    <Skills/>
+    <Projects/>
+    <Contact/>
+    <Connect/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Connect.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Connect extends React.Component {
+    render() {
+        return (
+            <>
+                <div className="connect" id="connect">
+                    <center>
+                        <div className="col-md-6">
+                            <a href="https://github.com/alihassan-here"><i className="bx bxl-github bx-md" /></a>
+                            <a href="https://web.facebook.com/profile.php?id=100009039556377"><i className="bx bxl-facebook bx-md" /></a>
+                            <a href="https://linkedin.com/ali-hassan-here"><i className="bx bxl-linkedin bx-md" /></a>
+                            <a href="https://twitter.com/ALIHassanhere"><i className="bx bxl-twitter bx-md" /></a>
+                        </div>
+                    </center>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Connect;
